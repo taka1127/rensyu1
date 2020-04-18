b3c828ed578720e62e89b03e5e9f7984a092ea16
taka1127 <58457640+taka1127@users.noreply.github.com>
2020-04-18 16:28:35 +0900

    form_02

--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) _
+        Handles Button1.Click
+        'ボタンをクリックしたときの処理
+        Label1.Text = "VBの世界へようこそ" 'ラベルに表示する文字設定
+        Label1.BackColor = Color.Black
+        Label1.ForeColor = Color.White
+    End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form1_Move(sender As Object, e As EventArgs) Handles MyBase.Move
+
+    End Sub
 End Class
