1764aa8d8594f5ca30a723a5294686587d1193a6
taka1127 <58457640+taka1127@users.noreply.github.com>
2020-04-20 12:53:43 +0900

    form_05

---
+++ joken1/joken1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim x1, x2 As Boolean
+        x1 = 10 > 20 And 20 < 40
+        x2 = 10 < 20 Or 20 < 40
+        MessageBox.Show(x1, "条件式10>20 And 20 < 40の結果")
+        MessageBox.Show(x2, "条件式10<20 Or 20 < 40の結果")
+    End Sub
+End Class
